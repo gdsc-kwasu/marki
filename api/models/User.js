493569e91589a9e7ef98cc21e9e857eba8d14fe9
ionware <ionwarez@gmail.com>
2020-01-22 10:13:54 +0100

    added hook and password validation to user Schema

--- api/models/User.js
+++ api/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema(
   {
@@ -22,6 +23,21 @@ const userSchema = new mongoose.Schema(
    */
   { timestamps: true }
 )
+2
+userSchema.pre('save', async function(next) {
+  const user = this
+  const hash = await bcrypt.hash(this.password, 10)
+  this.password = hash
+
+  next()
+})
+
+userSchema.methods.verifyPassword = async function(password) {
+  const user = this
+  const compared = bcrypt.compare(password, user.password)
+
+  return compared
+}
 
 const User = mongoose.model('user', userSchema)
 
